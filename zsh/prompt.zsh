0c3e5b75c961ff98c518cd806a0902d619959190
Mathijs Bakker <mathijs_bakker@me.com>
2020-08-25 11:39:36 +0200

    Add ZSH and TMUX configurations

---
+++ zsh/prompt.zsh
@@ -0,0 +1,45 @@
+autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
+setopt prompt_subst
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*' formats ' %b'
+
+add-zsh-hook precmd vcs_info
+add-zsh-hook precmd async_trigger
+
+source "$DOTFILES/zsh/git_prompt.zsh"
+source "$DOTFILES/zsh/jobs_prompt.zsh"
+
+PROMPT_SYMBOL='❯'
+
+ASYNC_PROC=0
+function async() {
+    printf "%s" "$(git_status) $(suspended_jobs)" > "/tmp/zsh_prompt_$$"
+
+    kill -s USR1 $$
+
+    if [[ "${ASYNC_PROC}" != 0 ]]; then
+        kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
+    fi
+}
+
+function async_trigger() {
+    ASYNC_PROC=$!
+    async &!
+}
+
+function TRAPUSR1() {
+    vcs_info
+    RPROMPT='$(cat /tmp/zsh_prompt_$$)'
+    ASYNC_PROC=0
+
+    zle && zle reset-prompt
+}
+
+precmd() {
+    print -P "\n%F{005}%~" 
+}
+
+export PROMPT='%(?.%F{006}.%F{009})$PROMPT_SYMBOL%f '
+export RPROMPT=''
