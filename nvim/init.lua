54090909500ce3e477b9ed398e170672f4a29dc9
Mathijs Bakker <mathijs_bakker@me.com>
2021-03-25 00:05:26 +0100

    Add Lsp stuff and File Sourcer

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,10 @@ end
 vim.g.mapleader = ','
 
 require'global_helpers'
+require'config'
 require'keymaps'
 require'packer.plugins'
 require'plugin.completion'
+require'lsp'
+
+vim.cmd(':source $HOME/.dotfiles/nvim/autoload/file_sourcer.vim')
