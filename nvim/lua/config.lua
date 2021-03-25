1ed1f83b92fedfe94dd99dcc6c5243155ce2394b
Mathijs Bakker <mathijs_bakker@me.com>
2021-03-25 10:40:55 +0100

    Update nvim config

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,5 +1,5 @@
 -- Option Scopes:
-local o = vim.o -- editor
+local o = vim.o -- editor global
 local bo = vim.bo -- buffer
 local wo = vim.wo -- window
 local cmd = vim.cmd
@@ -11,38 +11,34 @@ o.fileencoding = "utf-8"
 cmd('autocmd vimenter * colorscheme gruvbox')
 cmd('set termguicolors')
 
--- Rulers:
-wo.rnu = true
-wo.number = true -- set numbered lines
-
--- HL current line:
-wo.cursorline = true -- Enable highlighting of the current line
-
--- Visual line to mark 80 chars
+-- Colums, gutters and rulers:
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = "yes"
 wo.colorcolumn = "80,120"
+wo.cursorline = true
+o.scrolloff = 5
 
-cmd('set scrolloff=5')
-cmd('set mouse=nv')
+-- I smell a rat:
+o.mouse = "nv"
 
-cmd('set ts=4') -- Insert 2 spaces for a tab
-cmd('set sw=4') -- Change the number of space characters inserted for indentation
-bo.expandtab = true -- Converts tabs to spaces
+-- Use clipboard for ALL ops:
+o.clipboard = "unnamedplus"
+
+-- Tab Spacing:
+bo.tabstop = 4
+bo.softtabstop = 4
+bo.shiftwidth = 4
+o.shiftround = true
+bo.expandtab = true
+
 bo.smartindent = true -- Makes indenting smart
-o.showtabline = 2 -- Always show tabs
+-- o.showtabline = 2 -- Always show tabs
 o.showmode = false -- We don't need to see things like -- INSERT -- anymore
-o.backup = false -- This is recommended by coc
-o.writebackup = false -- This is recommended by coc
-wo.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+
+-- Performance:
 o.updatetime = 300 -- Faster completion
 --o.timeoutlen = 100
 
---[[
-filetype plugin indent on
-
-" Appearance: {{{
-
-    " Rulers: {{{
-        set rnu                             " Relative numbering
-        set nu                              " Line numbering
-    " }}}
---]]
+-- Avoid hit-enter caused by file messages:
+o.shortmess = "fmlwxsi"
