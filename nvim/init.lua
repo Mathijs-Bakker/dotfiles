8183cc38eaa5f5ac9fe3292b847075c3560112b0
Mathijs Bakker <mathijs_bakker@me.com>
2021-03-31 00:17:04 +0200

    Append lua/plugin namespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,7 @@ require'config.options'
 require'config.keymaps'
 require'packer.plugins'
 require'plugin.completion'
+require'plugin'
 require'lsp'
 
 vim.cmd(':source $HOME/.dotfiles/nvim/autoload/file_sourcer.vim')
