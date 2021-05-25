f7e15c8afc234094a09b6c8b29f3a0e9b8f3e21a
Mathijs Bakker <mathijs_bakker@me.com>
2021-05-25 10:18:43 +0200

    Add Yabai with sdhk and limelight.

---
+++ yabai/limelightrc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+limelight -m config width            2
+limelight -m config radius           12
+limelight -m config placement        exterior
+limelight -m config active_color     0xff775759
+limelight -m config normal_color     0xff555555
