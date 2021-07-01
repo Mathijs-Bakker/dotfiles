9b4a05a6f0bb935a2084e4e813842f47d7c77891
Mathijs Bakker <mathijs_bakker@me.com>
2021-07-01 16:14:43 +0200

    Teej broke my telescope. Fixed it.

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -13,7 +13,6 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
     prompt_prefix = "> ",
     selection_caret = "> ",
     entry_prefix = "  ",
@@ -21,7 +20,7 @@ require('telescope').setup{
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
+    layout_config = {
       horizontal = {
         width_padding = 0.1,
         height_padding = 0.1,
@@ -32,16 +31,14 @@ require('telescope').setup{
         height_padding = 1,
         preview_height = 0.5,
       },
+      preview_cutoff = 120,
+      prompt_position = "bottom",
     },
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
     color_devicons = true,
