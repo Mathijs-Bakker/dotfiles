c1ca72cbbcf21cbd656ef9e67c14ab66a118c485
Mathijs Bakker <mathijs_bakker@me.com>
2021-11-18 11:35:59 +0100

    Update rooter config

--- nvim/lua/plugins/rooter/init.lua
+++ nvim/lua/plugins/rooter/init.lua
@@ -1,12 +1,16 @@
 require'rooter'.setup {
-    manual = false,
-    echo = false,
-    patterns = {
+    manual = false, -- weather to setup autocommand to root every time a file is opened
+    echo = true, -- echo every time rooter is triggered
+    patterns = { -- the patterns to find
       '.git',    -- same as patterns passed to nvim_lsp.util.root_pattern(patterns...)
-      '.sln',
-      'Assets',
+      'Cargo.toml',
+      'go.mod',
     },
-    cd_command = 'lcd',
+    cd_command = 'lcd', -- the cd command to use, possible values are 'lcd', 'cd', and 'tcd'
+    -- what to do when the rooter pattern is not found
+    -- if this is 'current', will cd to the parent directory of current file
+    -- if this is 'home', will cd to the home directory
+    -- if this is 'none', will not do anything
     non_project_files = 'current',
 
     -- the start path to pass to nvim_lsp.util.root_pattern(patterns...)
