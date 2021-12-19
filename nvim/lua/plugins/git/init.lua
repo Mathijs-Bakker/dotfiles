7c1167490965fb9f78c2335cad3ab33c564e8061
Mathijs Bakker <mathijs_bakker@me.com>
2021-12-19 11:23:08 +0100

    Add fugitive keymap

--- nvim/lua/plugins/git/init.lua
+++ nvim/lua/plugins/git/init.lua
@@ -1,25 +1,30 @@
---[[ Fugitive: ]]--
+--[[ Fugitive: ]]
+--
 
 Nvim_exec([[ let g:fugitive_git_executable = 'hub' ]], true)
 
-Nnoremap('<Leader>gc', [[:Git commit -v<CR>]])
-Nnoremap('<Leader>gs', [[:Git<CR>]])
-Nnoremap('<Leader>ge', [[:Gedit<CR>]])
+Nnoremap('<Leader>ga', [[:Git add %:p]]) -- Git Add
+Nnoremap('<Leader>gc', [[:Git commit -v<CR>]]) -- Git Commit
+Nnoremap('<Leader>gs', [[:Git<CR>]]) -- Git Status
+Nnoremap('<Leader>ge', [[:Gedit<CR>]]) -- Git Edit
 Nnoremap('<Leader>gr', [[:Gread<CR>]])
 Nnoremap('<Leader>gb', [[:Git blame<CR>]])
 Nnoremap('<Leader>gp', [[:Git push<CR>]])
 
---[[ Fugitive-gitlab: ]]--
+--[[ Fugitive-gitlab: ]]
+--
 
 Nvim_exec([==[ let g:fugitive_gitlab_domains = ['https://my.gitlab.com']]==], true)
 
-TrySource'plugins.git.gitlab_access_token'
+TrySource 'plugins.git.gitlab_access_token'
 
---[[ Git Messenger: ]]--
+--[[ Git Messenger: ]]
+--
 
 -- Nnoremap('<leader>gm', [[<Plug>(git-messenger)]]) -- Default mapping
 
---[[ GV: ]]--
+--[[ GV: ]]
+--
 -- A 'git log --graph' wrapper
 
 Nnoremap('<Leader>gl', [[:GV<CR>]])
