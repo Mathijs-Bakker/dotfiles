3400a0946721fe8286d79e47da388a191b267978
Mathijs Bakker <mathijs_bakker@me.com>
2021-12-28 13:21:50 +0100

    Increase number of LSP hits in menu

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -53,7 +53,7 @@ cmp.setup {
   -- The order of the sources list defines the priority of each source.
   sources = cmp.config.sources {
     { name = 'luasnip' },
-    { name = 'nvim_lsp', max_item_count = 5 },
+    { name = 'nvim_lsp', max_item_count = 10 },
     { name = 'nvim_lua' },
     { name = 'custom_source' },
     { name = 'path' },
