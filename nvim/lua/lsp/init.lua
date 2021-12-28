8c0f387b463e6c02e14569a46a099220cd9f1ff5
Mathijs Bakker <mathijs_bakker@me.com>
2021-12-28 13:19:51 +0100

    Fix deprecated Nvim funcs

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -17,21 +17,26 @@ buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 -- Mappings.
 local opts = { noremap = true, silent = true }
 
-buf_set_keymap('n', 'gtD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-buf_set_keymap('n', 'gtd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-buf_set_keymap('n', 'gti', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+-- stylua: ignore start
+buf_set_keymap('n', 'gtD', 	        '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+buf_set_keymap('n', 'gtd', 	        '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+buf_set_keymap('n', 'gti', 	        '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 
-buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+buf_set_keymap('n', '<space>D',     '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
-buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+buf_set_keymap('n', 'K', 	        '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+buf_set_keymap('n', '<C-k>', 	    '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
-buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-buf_set_keymap('n', '<space>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-buf_set_keymap('n', 'dn', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-buf_set_keymap('n', 'dp', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+buf_set_keymap('n', '<space>wa',    '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+buf_set_keymap('n', '<space>wr',    '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+buf_set_keymap('n', '<space>wl',    '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+buf_set_keymap('n', '<space>rn',    '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+buf_set_keymap('n', 'gr', 	        '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+
+-- buf_set_keymap('n', '<space>d', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+buf_set_keymap('n', '<space>d',     '<Cmd>lua vim.diagnostic.show()<CR>', opts)
+buf_set_keymap('n', 'dn',           '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+buf_set_keymap('n', 'dp',           '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+buf_set_keymap('n', '<space>l',     '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+buf_set_keymap('n', '<space>qf',     '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
+-- stylua: ignore end
