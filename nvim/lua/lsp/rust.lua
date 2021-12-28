c3006b80b554974bb727b71ce5b77df0a2fd0d1c
Mathijs Bakker <mathijs_bakker@me.com>
2021-12-28 15:38:29 +0100

    On hover line diagnostics

--- nvim/lua/lsp/rust.lua
+++ nvim/lua/lsp/rust.lua
@@ -108,7 +108,20 @@ local opts = {
   -- all the opts to send to nvim-lspconfig
   -- these override the defaults set by rust-tools.nvim
   -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-  server = {},
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    -- on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ['rust-analyzer'] = {
+        -- enable clippy on save
+        -- checkOnSave = {
+        -- command = 'clippy',
+        -- },
+      },
+    },
+  },
 
   -- rust-analyer options
 
@@ -127,3 +140,6 @@ local opts = {
 }
 
 require('rust-tools').setup(opts)
+
+Cmd [[set updatetime=300]]
+Cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
