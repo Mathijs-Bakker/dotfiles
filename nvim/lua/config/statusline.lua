6a38a73a7cf1bd8ff015c6e080a5a8e4f304e2a7
Mathijs Bakker <mathijs_bakker@me.com>
2022-03-19 11:01:01 +0100

    Add 'global' statusline (nvim new feat: laststatus)

--- nvim/lua/config/statusline.lua
+++ nvim/lua/config/statusline.lua
@@ -1,14 +1,15 @@
+vim.opt.laststatus = 3
+
 local path_sep = require('plenary.path').path.sep
 
 local function get_relative_path_filename()
+  local path_head = vim.fn.expand '%:h'
+  -- print("ph " .. path_head)
+  if path_head ~= '' then
+    path_head = path_head .. path_sep
+  end
 
-    local path_head = vim.fn.expand('%:h')
-    -- print("ph " .. path_head)
-    if path_head ~= '' then
-        path_head = path_head .. path_sep
-    end
-
-    return '%#StatusLineNC# ' .. path_head .. '%*%#CursorLineNr#%t %*'
+  return '%#StatusLineNC# ' .. path_head .. '%*%#CursorLineNr#%t %*'
 end
 
 local function get_file_modified_status()
@@ -20,10 +21,7 @@ local function get_file_help_status()
 end
 
 local function get_file_status_section()
-  return string.format('%s %s %s',
-    get_relative_path_filename(),
-    get_file_modified_status(),
-    get_file_help_status())
+  return string.format('%s %s %s', get_relative_path_filename(), get_file_modified_status(), get_file_help_status())
 end
 
 local function get_lines()
@@ -31,25 +29,27 @@ local function get_lines()
 end
 
 local function get_lsp_status()
-  local status = require('plugins.lsp-status')
+  local status = require 'plugins.lsp-status'
   local diagnostics = status.statusline()
   return diagnostics
 end
 
 local function get_git_branch()
-  local git_branch = vim.fn["fugitive#head"]()
+  local git_branch = vim.fn['fugitive#head']()
 
-  if not git_branch or git_branch == '' then return '' end
+  if not git_branch or git_branch == '' then
+    return ''
+  end
 
   return '%#WildMenu#  ' .. git_branch .. '%*'
 end
 
-local harpoon_mark = require("harpoon.mark")
+local harpoon_mark = require 'harpoon.mark'
 
 local function get_harpoon_status()
   local mark_id = harpoon_mark.status()
 
-  if not mark_id or mark_id == "" then
+  if not mark_id or mark_id == '' then
     return '%#WildMenu# ♆  %*'
     -- return '%#WildMenu# ♆ -- %*'
   end
@@ -60,7 +60,9 @@ end
 local function is_terminal_window()
   local buftype = tostring(Bo.buftype)
 
-  if buftype == 'terminal' then return true end
+  if buftype == 'terminal' then
+    return true
+  end
 
   return false
 end
@@ -70,22 +72,20 @@ local separator = '  '
 local section_spacer = '%='
 
 function StatusLine()
-
   if is_terminal_window() then
-    statusline = '  Terminal  '
-    .. get_harpoon_status()
+    statusline = '  Terminal  ' .. get_harpoon_status()
   else
     statusline = separator
-    .. get_file_status_section()
-    .. separator
-    .. get_git_branch()
-    .. separator
-    .. get_harpoon_status()
-    .. section_spacer
-    .. get_lsp_status()
-    .. section_spacer
-    .. get_lines()
-    .. separator
+      .. get_file_status_section()
+      .. separator
+      .. get_git_branch()
+      .. separator
+      .. get_harpoon_status()
+      .. section_spacer
+      .. get_lsp_status()
+      .. section_spacer
+      .. get_lines()
+      .. separator
   end
 
   return statusline
