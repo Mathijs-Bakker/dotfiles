aba8b98212195debd3e4b48e2b755e08c0057692
Bob Rust <mathijs_bakker@me.com>
2022-09-20 20:33:05 +0200

    Add blank line plugin

--- nvim/lua/packer/plugins.lua
+++ nvim/lua/packer/plugins.lua
@@ -115,6 +115,17 @@ return require('packer').startup(function()
   use 'ellisonleao/gruvbox.nvim'
   -- use 'rebelot/kanagawa.nvim'
 
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require('indent_blankline').setup {
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end,
+  }
+
   -- Writing
   use 'junegunn/goyo.vim'
 
