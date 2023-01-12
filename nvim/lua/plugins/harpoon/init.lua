a812825f4ec167ad5268f9f123c7e3a0b2b0d86b
mathijs-bakker <mathijs_bakker@me.com>
2023-01-12 16:21:45 +0100

    Switch Grapple for Harpoon (Unexpected behaviour with different crates in one repo)

--- nvim/lua/plugins/harpoon/init.lua
+++ nvim/lua/plugins/harpoon/init.lua
@@ -18,3 +18,7 @@ Nnoremap(',3', [[:lua require('harpoon.term').sendCommand(1, 3)<CR>]])
 Nnoremap(',4', [[:lua require('harpoon.term').sendCommand(1, 4)<CR>]])
 -- Nnoremap('cr', [[:lua require('harpoon.term').sendCommand(1, 'cr')<CR>]])
 -- tf :lua require('harpoon.term').sendCommand(1, 'la')<CR>  tf :lua require('harpoon.term').sendCommand(1, 'la')<CR>
+
+-- require('harpoon').setup {
+-- mark_branch = true,
+-- }
