1fe7cc3b99a721b3992f1b99862a6505a9fc6f43
mathijs-bakker <mathijs_bakker@me.com>
2023-05-10 17:28:51 +0200

    Cargo run command for harpoon.

--- nvim/lua/lsp/rust.lua
+++ nvim/lua/lsp/rust.lua
@@ -1,8 +1,10 @@
--- vim.keymap.set('n', 'cr', ':! cargo run<CR>')
-vim.keymap.set('n', 'cr', ':TermExec cmd="cargo run"<CR>')
-vim.keymap.set('n', '<Leader>cb', ':TermExec cmd="cargo build"<CR>')
--- vim.keymap.set('n', '<Leader>ct', ':! cargo test<CR>')
-vim.keymap.set('n', '<Leader>ct', ':TermExec cmd="cargo test"<CR>')
+Nnoremap(
+  'cr',
+  [[:lua require('harpoon.term').sendCommand(1, 'cargo run\n')<CR> :lua require("harpoon.term").gotoTerminal(1)<CR>]]
+)
+
+vim.keymap.set('n', '<Leader>cb', ':! cargo build<CR>')
+vim.keymap.set('n', '<Leader>ct', ':! cargo test<CR>')
 vim.keymap.set('n', '<Leader>gd', ':RustOpenExternalDocs<CR>')
 vim.keymap.set('n', 'K', ':RustHoverActions<CR>')
 vim.keymap.set('n', '<Leader>k', ':RustMoveItemUp<CR>')
