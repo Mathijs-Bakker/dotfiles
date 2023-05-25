a7a5e1285d52bc6be3218bcb083efa8891500b5d
mathijs-bakker <mathijs_bakker@me.com>
2023-05-25 21:23:54 +0200

    Diagnostics: float window -> border, jump priority

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -3,9 +3,70 @@ TrySource 'lsp.omnisharp'
 TrySource 'lsp.typescript'
 TrySource 'lsp.viml'
 
+-- DIAGNOSTICS
 vim.diagnostic.config {
   virtual_text = true,
+  signs = true,
+  float = {
+    show_header = true,
+    border = 'rounded',
+    -- source = "always",
+    format = function(diagnostic)
+      if not diagnostic.code and not diagnostic.user_data then
+        return diagnostic.message
+      end
+
+      local t = vim.deepcopy(diagnostic)
+      local code = diagnostic.code
+      if not code then
+        if not diagnostic.user_data.lsp then
+          return diagnostic.message
+        end
+
+        code = diagnostic.user_data.lsp.code
+      end
+      if code then
+        t.message = string.format('%s [%s]', t.message, code):gsub('1. ', '')
+      end
+      return t.message
+    end,
+  },
+  severity_sort = true,
+  update_in_insert = false,
 }
+
+local severity_levels = {
+  vim.diagnostic.severity.ERROR,
+  vim.diagnostic.severity.WARN,
+  vim.diagnostic.severity.INFO,
+  vim.diagnostic.severity.HINT,
+}
+
+local get_highest_error_severity = function()
+  for _, level in ipairs(severity_levels) do
+    local diags = vim.diagnostic.get(0, { severity = { min = level } })
+    if #diags > 0 then
+      return level, diags
+    end
+  end
+end
+
+Nnoremap('<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>') -- Diagnostics
+Nnoremap('dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>') -- Diagnostics Previous
+vim.keymap.set('n', 'dp', function()
+  vim.diagnostic.goto_prev {
+    severity = get_highest_error_severity(),
+    wrap = true,
+    float = true,
+  }
+end)
+vim.keymap.set('n', 'dn', function()
+  vim.diagnostic.goto_next { severity = get_highest_error_severity(), wrap = true, float = true }
+end)
+Nnoremap('<Leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>') -- List Location List
+Nnoremap('<Leader>lf', '<cmd>lua vim.diagnostic.setqflist()<CR>') -- List quickFix
+
+-- End Diagnostics
 
 local function buf_set_option(...)
   vim.api.nvim_buf_set_option(0, ...)
@@ -34,15 +95,6 @@ Nnoremap('<Leader>wl', 	'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_f
 Nnoremap('<Leader>wa', 	'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
 Nnoremap('<Leader>wr', 	'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
 
--- Diagnostics:
-
-Nnoremap('<Leader>d', 	'<cmd>lua vim.diagnostic.open_float()<CR>') -- Diagnostics
-Nnoremap('dp', 		'<cmd>lua vim.diagnostic.goto_prev()<CR>') -- Diagnostics Previous
-Nnoremap('dn', 		'<cmd>lua vim.diagnostic.goto_next()<CR>') -- Diagnostics Next
--- ts_map('<Leader>dl', 'diagnostics')
--- Nnoremap('<Leader>ld', '<cmd>Telescope diagnostics<CR>') -- List Diagnotics
-Nnoremap('<Leader>ll', 	'<cmd>lua vim.diagnostic.setloclist()<CR>') -- List Location List
-Nnoremap('<Leader>lf', 	'<cmd>lua vim.diagnostic.setqflist()<CR>') -- List quickFix
 
 local whichkey = require 'which-key'
 whichkey.register {
