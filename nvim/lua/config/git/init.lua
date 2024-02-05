220b6d0cf3cc3b68e2ef625d0a215653d659df05
mathijs-bakker <mathijs_bakker@me.com>
2024-02-05 12:01:06 +0100

    Add 3-way diff keymaps

--- nvim/lua/config/git/init.lua
+++ nvim/lua/config/git/init.lua
@@ -3,12 +3,14 @@ vim.api.nvim_exec([[ let g:fugitive_git_executable = 'hub' ]], true)
 -- Nvim_exec([[ let g:fugitive_git_executable = 'hub' ]], true)
 
 Nnoremap('<Leader>ga', [[:Git add %:p<CR>]]) -- '$ git add' current file
+Nnoremap('<Leader>gb', [[:Git blame<CR>]]) -- Gitt Blame
 Nnoremap('<Leader>gc', [[:Git commit -v<CR>]]) -- Git Commit
-Nnoremap('<Leader>gs', [[:Git<CR>]]) -- Git Status
 Nnoremap('<Leader>ge', [[:Gedit<CR>]]) -- Git Edit
+Nnoremap('<Leader>gf', [[:Git pull --rebase<CR>]]) -- Git Pull/Fetch
+Nnoremap('<Leader>gl', [[:GV<CR>]]) -- Git Log
+Nnoremap('<Leader>gp', [[:Git push<CR>]]) -- Git Push
 Nnoremap('<Leader>gr', [[:Gread<CR>]])
-Nnoremap('<Leader>gb', [[:Git blame<CR>]])
-Nnoremap('<Leader>gp', [[:Git push<CR>]])
+Nnoremap('<Leader>gs', [[:Git<CR>]]) -- Git Status
 
 --[[ Fugitive-gitlab: ]]
 -- vim.api.nvim_exec([==[ let g:fugitive_gitlab_domains = ['https://my.gitlab.com']]==], true)
@@ -16,9 +18,11 @@ Nnoremap('<Leader>gp', [[:Git push<CR>]])
 -- require 'config.git.gitlab_access_token'
 
 --[[ Git Messenger: ]]
-Nnoremap('<leader>gm', [[<Plug>(git-messenger)]]) -- Default mapping
+Nnoremap('<leader>gm', [[<Plug>(git-messenger)]]) -- Git Blame under cursor
 
 --[[ GV: ]]
 -- A 'git log --graph' wrapper
 
-Nnoremap('<Leader>gl', [[:GV<CR>]])
+-- Resolving a 3-way diff with :diffget
+Nnoremap('gj', [[<cmd>diffget //2CR ]]) -- fetches the hunk from the target parent (on the left)
+Nnoremap('gk', [[<cmd>diffget //3CR ]]) -- fetches the hunk from the merge parent (on the right)
