16bc065a1983e7b429058795a59e89f6478ad920
mathijs-bakker <mathijs_bakker@me.com>
2024-02-06 19:32:15 +0100

    Add LSP config

---
+++ nvim/lua/lsp/omnisharp.lua
@@ -0,0 +1,21 @@
+local lsp_status = require 'plugins.lsp-status'
+
+local home = os.getenv 'HOME'
+local omnisharp_bin = home .. '/Repositories/language-servers/omnisharp-osx/run'
+local pid = vim.fn.getpid()
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+require('lspconfig').omnisharp.setup {
+  cmd = {
+    omnisharp_bin,
+    '--languageserver',
+    '--hostPID',
+    tostring(pid),
+  },
+  on_attach = lsp_status.on_attach,
+  -- capabilities = lsp_status.capabilities
+  capabilities = capabilities,
+}
