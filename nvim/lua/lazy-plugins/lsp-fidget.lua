54694ff1406465f46ee357a7d8ae308830f43d1c
mathijs-bakker <mathijs_bakker@me.com>
2024-02-14 10:43:32 +0100

    Override vim notifications with Fidget

---
+++ nvim/lua/lazy-plugins/lsp-fidget.lua
@@ -0,0 +1,17 @@
+return {
+  'j-hui/fidget.nvim',
+  -- tag = 'v1.3.0',
+  opts = {
+    -- options
+  },
+  config = function()
+    require('fidget').setup {
+      notification = {
+        -- poll_rate = 10, -- How frequently to update and render notifications
+        filter = vim.log.levels.INFO, -- Minimum notifications level
+        history_size = 256, -- Number of removed messages to retain in history
+        override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+      },
+    }
+  end,
+}
