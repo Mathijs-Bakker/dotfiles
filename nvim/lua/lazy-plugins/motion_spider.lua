b4fc1476475975eeab3a5788eded12dba18eff7a
mathijs-bakker <mathijs_bakker@me.com>
2024-02-22 15:22:09 +0100

    Add spider motion plugin

---
+++ nvim/lua/lazy-plugins/motion_spider.lua
@@ -0,0 +1,34 @@
+return {
+  'chrisgrieser/nvim-spider',
+  keys = {
+    { -- example for lazy-loading on keystroke
+      'e',
+      "<cmd>lua require('spider').motion('e')<CR>",
+      mode = { 'n', 'o', 'x' },
+    },
+    { -- example for lazy-loading on keystroke
+      'w',
+      "<cmd>lua require('spider').motion('w')<CR>",
+      mode = { 'n', 'o', 'x' },
+    },
+
+    { -- example for lazy-loading on keystroke
+      'b',
+      "<cmd>lua require('spider').motion('b')<CR>",
+      mode = { 'n', 'o', 'x' },
+    },
+
+    -- { -- example using an explicit function
+    --     'w',
+    --     function()
+    --       require('spider').motion('w', {
+    --         customPatterns = {
+    --           patterns = { ('%x'):rep(6) .. '+' },
+    --           overrideDefault = true,
+    --         },
+    --       })
+    --     end,
+    --     mode = { 'n', 'o', 'x' },
+    --   },
+  },
+}
