73dfc259d7b08370000d476961617fcfac98c595
mathijs-bakker <mathijs_bakker@me.com>
2024-10-04 11:29:37 +0200

    Update Lazy command

--- nvim/lua/lazy-plugins/greeter.lua
+++ nvim/lua/lazy-plugins/greeter.lua
@@ -19,7 +19,7 @@ return {
       dashboard.button('<Ldr> fp', '﯑ Projects', ':Telescope projects<CR>'),
       dashboard.button('<Ldr> fw', ' Grep Word', ':Telescope grep_string<CR>'),
       dashboard.button('e', ' New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('up', ' Update Plugins', ':PackerUpdate <CR>'),
+      dashboard.button('up', ' Update Plugins', ':Lazy <CR>'),
       dashboard.button('ch', '﫵Check Health', ':checkhealth <CR>'),
     }
 
