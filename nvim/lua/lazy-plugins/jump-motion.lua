78b666cd7a5a084d1008121457e0542bb41f2ccd
mathijs-bakker <mathijs_bakker@me.com>
2025-11-07 09:30:11 +0100

    Fix: deprecated jump motion func

--- nvim/lua/lazy-plugins/jump-motion.lua
+++ nvim/lua/lazy-plugins/jump-motion.lua
@@ -2,6 +2,23 @@ return {
   'ggandor/leap.nvim',
   dependencies = 'tpope/vim-repeat',
   config = function()
-    require('leap').create_default_mappings()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+    require('leap').opts.preview = function(ch0, ch1, ch2)
+      return not (ch1:match '%s' or (ch0:match '%a' and ch1:match '%a' and ch2:match '%a'))
+    end
+
+    -- Define equivalence classes for brackets and quotes, in addition to
+    -- the default whitespace group:
+    require('leap').opts.equivalence_classes = {
+      ' \t\r\n',
+      '([{',
+      ')]}',
+      '\'"`',
+    }
+
+    -- Use the traversal keys to repeat the previous motion without
+    -- explicitly invoking Leap:
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
   end,
 }
