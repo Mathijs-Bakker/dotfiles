76ad02371d9c55767d8fd17ee59f01deab5a3c67
mathijs-bakker <mathijs_bakker@me.com>
2026-03-24 17:22:50 +0100

    Add codex

---
+++ nvim/lua/lazy-plugins/codex.lua
@@ -0,0 +1,13 @@
+return {
+  'eskalVAR/codex.nvim',
+  opts = {
+    width = 0.48,
+    personality = 'pragmatic',
+    -- Keymaps
+    toggle_key = '<leader>cc',
+    quit_key = '<C-q>',
+    send_key = '<CR>',
+    attach_file_key = '<C-f>',
+    interrupt_key = '<C-c>',
+  },
+}
