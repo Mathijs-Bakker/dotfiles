46bcda4ec7cff79c651e78fb86824e9d43208587
mathijs-bakker <mathijs_bakker@me.com>
2026-03-24 17:20:35 +0100

    Markdown preview

--- nvim/lua/lazy-plugins/markdown_preview.lua
+++ nvim/lua/lazy-plugins/markdown_preview.lua
@@ -1,9 +1,14 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && npm install',
-  init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
-  ft = { 'markdown' },
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = { 'markdown', 'codecompanion' },
 }
+
+-- return {
+--   'iamcco/markdown-preview.nvim',
+--   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+--   build = 'cd app && npm install',
+--   init = function()
+--     vim.g.mkdp_filetypes = { 'markdown' }
+--   end,
+--   ft = { 'markdown' },
+-- }
